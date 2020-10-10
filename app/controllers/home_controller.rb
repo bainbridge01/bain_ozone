f05618543146743404f2213e54bcb12d00663f4a
bainbridge01 <engr.bainbridge@gmail.com>
2020-10-10 23:30:05 +0000

    color logic for jumbotron

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,30 @@ class HomeController < ApplicationController
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
    
-   #check for emptyreturn resu 
+   #check for emptyreturn result
     if @output.empty?
       @final_output = "Error"
     else
       @final_output = @output[0]['AQI']
     end
     
+    
+    if @final_output == "Error"
+      @api_color = "grey"
+    elsif @final_output <= 50
+      @api_color = "green"
+    elsif @final_output >=51 && @final_output <= 100
+      @api_color = "yellow"
+    elsif @final_output >=101 && @final_output <= 150
+      @api_color = "orange"
+    elsif @final_output >=151 && @final_output <= 200
+      @api_color = "red"
+    elsif @final_output >=201 && @final_output <= 300
+      @api_color = "purple"
+    elsif @final_output >=301 && @final_output <= 500
+      @api_color = "marron"
+    end
+      
     
   end
 end
