e045b6501367e048ca2a760274dfb7aec9203480
bainbridge01 <engr.bainbridge@gmail.com>
2020-10-10 20:46:47 +0000

    connecting API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=0&API_KEY=CB733BFA-EE0D-4E77-BDFB-721C683A708A'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+    
   end
 end
